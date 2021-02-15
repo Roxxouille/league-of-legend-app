67b5a93f12c17f7de9a66bbb8cd7f2b8784ee82b
Roxxouille <raphael.huiban.le.corff@gmail.com>
2021-02-15 23:04:53 +0100

    fixed html issue in champDetail, ajusted css landingPage

--- components/ChampDetails.js
+++ components/ChampDetails.js
@@ -2,7 +2,7 @@ const ChampDetails = ({ spell }) => {
     return (
         <div className="text-center bg-gray-900 p-2 rounded-xl w-1/2 mb-4">
             <h3 className="text-xl">{spell.name}</h3>
-            <p>{spell.description}</p>
+            <p dangerouslySetInnerHTML={{ __html: spell.description}}/>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,26 +14,26 @@ export default function Home({ champsProps }) {
   }, [search])
 
   return (
-    <Flipper flipKey={champs} className="bg-black min-h-screen">
-      <div className="p-10 flex flex-wrap justify-around">
+    <Flipper flipKey={champs} className="bg-black min-h-screen w-auto">
+      <div className="p-10 flex flex-wrap justify-center">
         <div className="w-full m-2 flex justify-center">
-          <input className="rounded bg-gray-100 p-2 font-bold" placeholder="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+          <input className="bg-gray-100 p-2 font-bold outline-none border-black border-8 rounded  focus:border-gray-900" placeholder="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
         </div>
         {champs && champs.map(champ => {
           return (
             <Flipped key={champ.id} flipId={champ.id}>
-              <div className="text-center p-2 font-mono font-bold text-m bg-gray-900 m-2 rounded-xl text-white">
-                <Link href={`/champ/${champ.id}`}>
+              <Link href={`/champ/${champ.id}`}>
+                <div className="text-center p-2 font-mono font-bold text-m bg-gray-900 m-2 rounded-xl text-white cursor-pointer hover:bg-white hover:text-black">
                   <a>
                     <img src={`http://ddragon.leagueoflegends.com/cdn/11.3.1/img/champion/${champ.image.full}`} />{champ.name}
                   </a>
-                </Link>
-              </div>
+                </div>
+              </Link>
             </Flipped>
           );
         })}
       </div>
-    </Flipper>
+    </Flipper >
   )
 }
 
